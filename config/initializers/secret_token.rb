ae2631474005227f6a8fe273ced2dab09ea47e25
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-03-16 07:45:29 +0800

    Start of something big.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ppm::Application.config.secret_token = '5aa35be3ff81340c463f47bd41741ef7d21372440cc96b13292890119163993444335c918b4784b7f59ddbc148523056885fa9044a1dc70a7debe01b373712d5'
