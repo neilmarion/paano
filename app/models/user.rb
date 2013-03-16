85d00a4a9ba1cf8fcba9eac69457c60c07715975
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-03-16 08:59:11 +0800

    Changes on user.rb to pass the currently failing tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :provider, :uid
+
+  validates_presence_of :name
+  validates_presence_of :provider
+  validates_presence_of :uid
+
+
+
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,10 +1,8 @@
 require 'spec_helper'
 
 describe User do
-  it { should validate_presence_of :email }
-  it { should validate_presence_of :password }
+  it { should validate_presence_of :provider }
+  it { should validate_presence_of :uid } 
+  it { should validate_presence_of :name }
 
-  it "should validate format of email" do
-    FactoryGirl.create(:user)
-  end
 end
