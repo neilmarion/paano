bfbbaf5aaf3cbbd5d291ab092105f6bc6e95e934
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-03-16 17:54:48 +0800

    Added integration specs for signing out

---
+++ spec/features/sign_out_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "Signing out" do
+  it "successfully signs out" do
+    visit root_path
+    click_link "Sign in with Facebook"
+    page.should have_link "Sign out"
+    click_link "Sign out"
+    page.should have_link "Sign in with Facebook"
+    page.should have_link "Sign in with Twitter"
+  end
+
+end
