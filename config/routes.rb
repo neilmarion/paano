a022b815d3a5080232b1eb281bbff8eecc176508
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-03-16 10:05:43 +0800

    Added questions controller, removed public/index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Ppm::Application.routes.draw do
+  root :to => 'questions#index'
+  resources :questions  
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
