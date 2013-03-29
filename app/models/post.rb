f93407d3a13d02414488df2447bcd1fdf613af75
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-03-30 04:00:57 +0800

    Pending search specs and text_search query order by ts_ranks * reputations.value

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,14 +3,15 @@ class Post < ActiveRecord::Base
 
   belongs_to :user
 
+  scope :join_rs_reputations, :joins => "LEFT JOIN rs_reputations ON posts.id = rs_reputations.target_id AND rs_reputations.reputation_name = 'votes' AND rs_reputations.active = 't'"
+
   def self.text_search(query)
     if query.present?
       rank = <<-RANK
         ts_rank(to_tsvector(title), plainto_tsquery(#{sanitize(query)})) +
         ts_rank(to_tsvector(content), plainto_tsquery(#{sanitize(query)}))
       RANK
-      where("to_tsvector('english', title) @@ plainto_tsquery(:q) or
-        to_tsvector('english', content) @@ plainto_tsquery(:q)", q: query).order("#{rank} desc")
+      select("*").join_rs_reputations.where("to_tsvector('english', title) @@ plainto_tsquery(:q) or to_tsvector('english', content) @@ plainto_tsquery(:q)", q: query).order("(rs_reputations.value * (#{rank})) desc")
     else
       scoped
     end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,8 +4,13 @@ describe PostsController do
 
   describe "GET 'index'" do
     before(:each) do
-      @question_1 = FactoryGirl.create(:question, content: "Question Exact")
+      @question_1 = FactoryGirl.create(:question, content: "Question Exactly Michael Jackson")
       @question_2 = FactoryGirl.create(:question)
+      @question_3 = FactoryGirl.create(:question, content: "Question Exact This is")
+
+      @user_1 = FactoryGirl.create(:user_facebook)
+      
+      @question_1.add_evaluation(:votes, 15, @user_1)
     end
   
     it "returns http success" do
@@ -15,16 +20,17 @@ describe PostsController do
 
     it "returns search results" do
       get :index, { query: "Question" }
-      assigns(:posts).should eq [@question_1, @question_2]
+      assigns(:posts).should eq [@question_1, @question_2, @question_3]
     end
 
     it "returns exact search results" do
-      get :index, { query: "Exact" }
+      get :index, { query: "Exactly Michael Jackson" }
       assigns(:posts).should eq [@question_1]
     end
 
     it "returns results according to reputation * relevance scores" do
-      pending
+      get :index, { query: "Question Exact" } 
+      assigns(:posts).should eq [@question_1, @question_3]
     end
 
   end
