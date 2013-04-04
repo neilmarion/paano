7176b78e0453f50aea49623f294804f45bd44277
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-04-04 14:54:59 +0800

    Removed application controller on overriding my current_user

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,6 +1,5 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def all
-    raise session.to_json
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
       flash.notice = "Signed in!"
