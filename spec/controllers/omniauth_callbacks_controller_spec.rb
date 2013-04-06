f2aa0c03c5b8407182b3e076257a39d523f596cc
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-04-06 10:46:53 +0800

    Some specs for omniauth_callbacks_controller

--- spec/controllers/omniauth_callbacks_controller_spec.rb
+++ spec/controllers/omniauth_callbacks_controller_spec.rb
@@ -1,15 +1,26 @@
 require 'spec_helper'
 
 describe Users::OmniauthCallbacksController do
-
-  describe 'invalid provider' do
-    it "fails" do
+  describe 'fail' do
+    before(:each) do
       @request.env["devise.mapping"] = Devise.mappings[:user]
-      request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:bad_provider]
-      get :facebook
-      response.should_not be_success
+    end
+
+    describe 'invalid user' do
+      it 'fails' do
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:facebook]
+        #User.any_instance.expects(:persisted?).returns false
+        get :facebook
+        response.should redirect_to root_path 
+      end
+    end
+
+    describe 'invalid provider' do
+      it "fails" do
+        request.env["omniauth.auth"] = OmniAuth.config.mock_auth[:bad_provider]
+        get :facebook
+        response.should_not be_success
+      end
     end
   end
-
-
 end
