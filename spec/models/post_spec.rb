8843790c9a6178a54355081d174355212592ba88
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-04-06 11:24:18 +0800

    Created tests for post.rb

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,4 +2,23 @@ require 'spec_helper'
 
 describe Post do
   it { should belong_to :user }
+
+  describe 'text_search' do
+    describe 'success' do
+      let(:post){ FactoryGirl.create(:question) }
+      it 'returns results' do
+        Post.text_search(post.title).should eq [post]
+      end
+
+      it 'returns no results' do
+        Post.text_search("xxx").should eq []
+      end
+  
+      describe 'query not present' do
+        it 'returns all results' do
+          Post.text_search(nil).should eq [post]
+        end
+      end
+    end
+  end
 end
