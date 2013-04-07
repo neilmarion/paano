84ad99eb8f10a33b18ef9829ee41665917d291fb
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-04-08 07:50:19 +0800

    Fixed tests and il8n for validation of tags

--- spec/support/post_spec.rb
+++ spec/support/post_spec.rb
@@ -1,7 +1,6 @@
 shared_examples "a post" do
   it { should belong_to :user }
   it { should validate_presence_of :content }
-  it { should have_many :taggings }
-  it { should validate_presence_of(:taggings).
-    with_message(I18n.t('activerecord.errors.models.post.attributes.taggings.blank')) }
+  it { should validate_presence_of(:tag_list).
+    with_message(I18n.t('activerecord.errors.models.post.attributes.tag_list.blank')) }
 end
