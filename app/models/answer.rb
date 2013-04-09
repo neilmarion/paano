837c915b700e4f7c312c0daef3b0a1a2dba66a58
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-04-09 19:15:29 +0800

    Updated seeds

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,4 +1,5 @@
 class Answer < Post
+  attr_accessible :question
   has_reputation :votes, :source => :user
   belongs_to :question, :foreign_key => "question_id"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -14,10 +14,16 @@ file = File.read(
   File.expand_path(File.join(File.dirname(__FILE__), "development_answers_data.json")))
 answers = ActiveSupport::JSON.decode(file)
 
-for i in 1..20
+def create_user(uid)
   token = Devise.friendly_token[0,20]
-  user = User.create(provider: "facebook", uid: i, name: Faker::Name.name,
+  User.create(provider: "facebook", uid: uid, name: Faker::Name.name,
     email: Faker::Internet.email, password: token, password_confirmation: token)
-  question = user.questions.create(title: questions[i-1]['title'], content: questions[i-1]['content'], tag_list: Faker::Name.first_name)
-  question.answers.create(content: answers[i-1]['content'], tag_list: Faker::Name.first_name)
 end
+
+for i in 1..20
+  question = create_user(i).questions.create(title: questions[i-1]['title'], content: questions[i-1]['content'], tag_list: Faker::Name.first_name)
+  create_user(i).answers.create(content: answers[i-1]['content'], tag_list: Faker::Name.first_name,
+    question: question)
+end
+
+
