6d2357ee45ddf5defbc2359a6255ec8c21371cd7
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-04-11 17:25:07 +0800

    Added a home layout

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  layout 'home', only: :index
   before_filter :authenticate_user!, except: [:index, :show]
   def index
     @posts = Post.text_search(params[:query]).page(params[:page]).per(PAGINATION['posts_index'])
