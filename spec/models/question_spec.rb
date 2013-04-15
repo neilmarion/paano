a3aab1db3107519283a1a8051a04819635dbd339
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-04-15 15:58:10 +0800

    Reloading the model after incrementing count

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -27,7 +27,7 @@ describe Question do
       expect{
         expect{
           @question.answers.create(content: "Answer content", tag_list: "tag_1, tag_2", user: FactoryGirl.create(:user_facebook))
-          puts @question.inspect
+          @question.reload
         }.to change(Answer, :count).by 1
        }.to change(@question, :answers_count).by 1
 
