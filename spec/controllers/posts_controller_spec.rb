6369a2bcf63d941c42af3a673dd10a6d4e4c9d0b
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-04-16 11:20:10 +0800

    Brought back the full-text search specs

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -27,16 +27,26 @@ describe PostsController do
       assigns(:posts).should eq []
     end
 
-    #it "returns (plainly) by significance (ts_rank)" do
-    #  get :index, { query: "hiccup" }
-    #  assigns(:posts).should eq [@posts[7], @posts[27]]
-    #end
+    describe "full-text search" do
+      before(:each) do
+        @query = "Magnanimous"
+        word = "Other"
+        @post_1 = FactoryGirl.create(:question_with_an_answer, title: @query, content: @query, tag_list: @query)         
+        @post_2 = FactoryGirl.create(:question_with_an_answer, title: word, content: @query, tag_list: @query)
+        @post_3 = FactoryGirl.create(:question_with_an_answer, title: @query, content: @query, tag_list: @query)
+      end
 
-    #it "returns (plainly) by reputation values" do
-    #  question = FactoryGirl.create(:question, title: @posts[1].title, content: @posts[1].content)
-    #  question.add_evaluation(:votes, 10, user_1)
-    #  get :index, { query: "fingerprint" }
-    #  assigns(:posts).should eq [question, @posts[1], @posts[21]]
-    #end
+      it "returns (plainly) by significance (ts_rank)" do
+        get :index, { query: @query }
+        assigns(:posts).should eq [@post_1, @post_3, @post_2]
+      end
+
+      it "returns (plainly) by reputation values" do
+        Post.destroy(@post_2.id)
+        @post_1.add_evaluation(:votes, 10, user_1)
+        get :index, { query: @query }
+        assigns(:posts).should eq [@post_1, @post_3]
+      end
+    end
   end
 end
