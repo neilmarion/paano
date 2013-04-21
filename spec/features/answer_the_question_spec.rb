523d1d969f8af13616b95f8a90a8097eb7f61c81
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-04-21 08:18:19 +0800

    Added feature spec testing if an answer is gonna be created if content is blank

--- spec/features/answer_the_question_spec.rb
+++ spec/features/answer_the_question_spec.rb
@@ -8,13 +8,21 @@ describe "Answering a question in the question show page" do
     current_path.should eq question_path(@question)
   end
   
-  it "fails if user is not yet signed in" do
-    expect{ 
-      fill_in :question_answers_attributes_0_content, with: "My Answer" 
-      click_button I18n.t('questions.submit.post')
-    }.to_not change(Answer, :count)
-  end
+  describe "fails" do
+    it "if user is not yet signed in" do
+      expect{ 
+        fill_in :question_answers_attributes_0_content, with: "My Answer" 
+        click_button I18n.t('questions.submit.post')
+      }.to_not change(Answer, :count)
+    end
 
+    it "if answer content is blank" do
+      expect{ 
+        click_button I18n.t('questions.submit.post')
+      }.to_not change(Answer, :count)
+    end
+  end
+  
   it "succeeds if user is signed in" do
     click_link I18n.t('shared.navbar.user_links.sign_in_with_fb_link.sign_in_with_facebook')
     click_link @question.title
