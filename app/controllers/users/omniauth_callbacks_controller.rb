efbd299d1a96d0544307d29d757ff809c5938eed
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-04-23 11:22:03 +0800

    Reverted back signing in devise

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,12 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     user = User.from_omniauth(request.env["omniauth.auth"])
     if user.persisted?
       flash.notice = "Signed in!"
-      if request.env['HTTP_REFERER'] 
-        sign_in user
-        redirect_to request.env['HTTP_REFERER']
-      else
-        sign_in_and_redirect user
-      end
+      sign_in_and_redirect user
     else
       session["devise.user_attributes"] = user.attributes
       redirect_to new_user_session_path
