59ace18450f5c65fc8bc9ae2cb6fe7946a12b7e3
Neil Marion dela Cruz <neilmarion@freelancer.com>
2013-04-25 13:31:58 +0800

    try

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Ppm::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w[*.png *.jpg *.jpeg *.gif]  
+  config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)  
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
