cea97103fb99580263da67356ca774255c4adb03
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-05-01 14:09:04 +0800

    Some changes on the config files

--- config/unicorn_staging.rb
+++
@@ -1,9 +0,0 @@
-root = "/home/deployer/apps/paanouplb_staging/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.paanouplb_staging.sock"
-worker_processes 2
-timeout 30
