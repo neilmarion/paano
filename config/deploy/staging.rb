b636ae1055ed720fe565765effb0cb2fbc82c5b4
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-05-01 11:07:23 +0800

    multistaging files

---
+++ config/deploy/staging.rb
@@ -0,0 +1,38 @@
+server "www.staging.uplb.paano.info"
+
+set :branch, "staging"
+set :application, "paanouplb_staging"
+set :deploy_to, "/home/#{user}/apps/#{application}"
+
+namespace :deploy do
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
+    end 
+  end 
+
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx_staging.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init_staging.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end 
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end 
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/staging`
+      puts "WARNING: HEAD is not the same as origin/staging"
+      puts "Run `git push` to sync changes."
+      exit
+    end 
+  end 
+  before "deploy", "deploy:check_revision"
+end
