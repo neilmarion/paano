cb40b17b9008e78caf6ef9c49583f1dcfc2f99e1
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-05-01 14:20:50 +0800

    changed

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -12,9 +12,9 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/deployer/apps/paanouplb/current
+APP_ROOT=/home/deployer/apps/paanouplb_staging/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E staging"
 AS_USER=deployer
 set -u
 
