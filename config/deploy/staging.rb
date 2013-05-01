f60e2f77e40dcc64af0749aa46c096e483e75ff0
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-05-01 13:57:59 +0800

    Some changes

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -12,8 +12,8 @@ namespace :deploy do
   end 
 
   task :setup_config, roles: :app do
-    sudo "ln -nfs #{current_path}/config/nginx_staging.conf /etc/nginx/sites-enabled/#{application}"
-    sudo "ln -nfs #{current_path}/config/unicorn_init_staging.sh /etc/init.d/unicorn_#{application}"
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
     puts "Now edit the config files in #{shared_path}."
