8fddc5c150954ed7e7d5453f7b8e120d23984b09
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-05-09 23:18:23 +0800

    Added spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,9 +6,15 @@ describe User do
   it { should have_many :questions }
   it { should have_many :answers }
 
-  it "should give user's reputation" do
+  it "should give user's reputation when a question is voted on" do
     question = FactoryGirl.create(:question)    
     question.add_evaluation(:votes, 1, FactoryGirl.create(:user_facebook))
     question.user.karma.should eq question.reputation_for(:votes)
+  end
+
+  it "should give user's reputation when an answer is voted on" do
+    answer = FactoryGirl.create(:answer)    
+    answer.add_evaluation(:votes, 1, FactoryGirl.create(:user_facebook))
+    answer.user.karma.should eq question.reputation_for(:votes)
   end
 end
