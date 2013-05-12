a90ba2cd90093f3047f6e6e236dd3c4b87ceba8b
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-05-12 10:39:56 +0800

    Edited production and staging deploy files

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -16,12 +16,14 @@ namespace :deploy do
   task :setup_config, roles: :app do
     run "mkdir -p #{shared_path}/config"
     put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/facebook.example.yml"), "#{shared_path}/config/facebook.yml"
     puts "Now edit the config files in #{shared_path}."
   end 
   after "deploy:setup", "deploy:setup_config"
 
   task :symlink_config, roles: :app do
     run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/facebook.yml #{release_path}/config/facebook.yml"
   end 
   after "deploy:finalize_update", "deploy:symlink_config"
 
