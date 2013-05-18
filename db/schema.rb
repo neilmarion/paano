51927586b59fe76e3d577370503cd64b5a9033bc
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-05-18 17:44:15 +0800

    Gathering user education

--- db/schema.rb
+++ db/schema.rb
@@ -106,6 +106,8 @@ ActiveRecord::Schema.define(:version => 20130513134820) do
     t.string   "username"
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "college"
+    t.string   "concentration"
     t.datetime "created_at",                          :null => false
     t.datetime "updated_at",                          :null => false
     t.string   "email",               :default => "", :null => false
