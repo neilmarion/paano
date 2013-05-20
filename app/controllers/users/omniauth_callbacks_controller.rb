340c42029fb8c8f1203e354385706abaf7a5cdf3
Neil Marion F. dela Cruz <nmfdelacruz@gmail.com>
2013-05-20 18:48:31 +0800

    Fixes to bug #105 & #106

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,8 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+
   def all
     user = User.from_omniauth(request.env["omniauth.auth"])
+
     if user.persisted?
       flash.notice = "Signed in!"
       sign_in_and_redirect user
@@ -9,5 +11,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_session_path
     end
   end
+
   alias_method :facebook, :all
+
 end
